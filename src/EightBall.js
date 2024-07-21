ba4c12835899a7dda7381a9832a2343e3393d7da
John Bonilla <jbonilla@neworg.com>
2024-07-21 18:49:58 -0400

    Finished

---
+++ src/EightBall.js
@@ -0,0 +1,34 @@
+﻿import './EightBall.css';
+import './random'
+import getRandomAnswer from "./random";
+import {useState} from "react";
+
+/**
+ * EightBall component that displays a magic eight ball.
+ * On clicking the eight ball, it shows a random answer from the provided answers.
+ * Includes a reset button to reset the message to the initial state.
+ *
+ * @param {Array} answers - The array of possible answers.
+ * @returns {JSX.Element} The EightBall component.
+ */
+function EightBall({answers}) {
+
+    const initialMessage = {msg: 'Think of a Question', color: 'white'};
+    const [message, setMessage] = useState(initialMessage);
+
+    function resetMessage() {
+        setMessage(initialMessage);
+    }
+
+    return (
+        <div className="container">
+            <div className="EightBall"
+                 onClick={() => setMessage(getRandomAnswer(answers))}>
+                <b className="question"
+                   style={{borderColor: message.color, color: message.color}}>{message.msg}</b>
+            </div>
+            < button className="resetButton" onClick={resetMessage}> Reset< /button>
+        </div>);
+}
+
+export default EightBall;
---
+++ src/random.js
@@ -0,0 +1,13 @@
+﻿// This module exports a function that returns a random answer from the provided array of answers.
+
+/**
+ * Selects a random answer from the provided array of answers.
+ * @param {Array} answers - The array of possible answers.
+ * @returns {Object} A random answer object containing a message and a color.
+ */
+function getRandomAnswer(answers) {
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    return answers[randomIndex];
+}
+
+export default getRandomAnswer;
